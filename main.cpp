447abc15cf32ae8d2a2af06e07827e6db7b4b8aa
Michael Wu <38211512+mwu412@users.noreply.github.com>
2018-06-04 18:24:03 +0800

    Worked

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main() {
+
+	//ifstream file("PeriodicTable_V2.csv");
+	ifstream file;
+	file.open("PeriodicTable_V2.csv");
+	if (file.is_open()) {
+		while (file.good()) {
+			string element;
+			double mass;
+			getline(file, element, ',');	//getline will stop when it see ',' (in csv: column1,column2,...)     
+			//getline only takes string;
+			file >> mass;	
+			file.ignore();	//ignore '\n'
+			cout << "map[" << element << "] = " << mass << ";" << endl;
+		}
+	}
+
+	file.close();
+
+	system("pause");
+	return 0;
+}
